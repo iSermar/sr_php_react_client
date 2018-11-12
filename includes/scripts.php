bd3fbed1e1fe8943757d692b2e6cf7d08e9b9d5d
Sermar De Sousa <sermarruiz@gmail.com>
2018-11-12 08:07:17 -0200

    First commit.

---
+++ includes/scripts.php
@@ -0,0 +1,21 @@
+    <!-- react js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.34/browser.min.js"></script>
+    
+    <!-- react code -->
+    <script type="text/babel" src="app/product/update_product.component.js"></script>
+    <script type="text/babel" src="app/product/create_product.component.js"></script>
+    <script type="text/babel" src="app/product/read_one_product.component.js"></script>
+    <script type="text/babel" src="app/product/delete_product.component.js"></script>
+    <script type="text/babel" src="app/product/product_table_row.component.js"></script>
+    <script type="text/babel" src="app/product/product_table.component.js"></script>
+    <script type="text/babel" src="app/product/top_actions.component.js"></script>
+    <script type="text/babel" src="app/product/read_products.component.js"></script>
+    <script type="text/babel" src="app/main.component.js"></script>
+    
+    <!-- jQuery library required by bootsrap js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    
+    <!-- bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'includes/head.php' ?>
+</head>
+
+<body>
+
+    <!-- container -->
+    <div class="container">
+
+        <div class="page-header">
+            <h1>Loading...</h1>
+        </div>
+
+        <!-- placeholder for rendering react components -->
+        <div id='content'></div>
+
+    </div>
+    <!-- /container -->
+
+    <?php include 'includes/scripts.php' ?>
+
+</body>
+
+</html>
